9864b7abbaa7313f698c3a1c941d60dbddeacdd6
Lucas <lucassanntos01@gmail.com>
2022-03-25 05:07:17 -0300

    set up router

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <router-link to="/">Notes</router-link> | 
+  <router-link to="/stats">Strts</router-link>
+
+  <router-view></router-view>
+</template>
+
+<script>
+export default {
+
+  name: 'App',
+
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
+
+<style lang="css" scoped>
+</style>
